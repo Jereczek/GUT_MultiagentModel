041962746b5a216169d4e10b1488984af7dbf06b
Dominikus <dominikus-noah@live.de>
2022-06-04 18:20:09 +0200

    Fixed unit test discovery

--- suswa_test/agent_test.py
+++ suswa_test/agent_test.py
@@ -8,12 +8,12 @@ from suswa.suswalib.agents.agent import *
 
 class AgentUnitTests(unittest.TestCase):
 
-    def selftest(self):
+    def test_selftest(self):
         self.assertTrue(True)
 
-    def exists(self):
+    def test_exists(self):
         a = Agent()
         pass
 
-if __name__ == '__main__':
-    unittest.main()
+# if __name__ == '__main__':
+#     unittest.main()
