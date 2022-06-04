a95db2db9a1a8baf5f96c3cb9bd594ac493ff6be
Dominikus <dominikus-noah@live.de>
2022-06-04 18:52:02 +0200

    WIP

--- suswa/suswalib/systems/rae.py
+++ suswa/suswalib/systems/rae.py
@@ -1,6 +1,15 @@
+import numpy as np
+
 class Rae:
-    """Reputation aggregation engine"""
+    """
+    Reputation aggregation engine
     
-    def __init__(self):
+    Aggregates agents' reputation data and calculates their trustworthiness.
+    """
+
+    def __init__(self, number_of_agents: int, initial_reputation: float = 1.0):
+        self.__number_of_agents = number_of_agents
+        self.__trustworthiness_matrix = np.ones((number_of_agents, number_of_agents)) * initial_reputation
         pass
+
     
---
+++ suswa/suswalib/systems/rtbs.py
@@ -0,0 +1,7 @@
+class Rtbs:
+    """
+    Reputation and trust building scheme.
+    """
+
+    def __init__(self):
+        pass
